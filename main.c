96f94f74bda28c903931f3a0940f926eba3a6aab
zxc8292859-commits <zxc8292859@gmail.com>
2026-02-09 14:50:40 +0800

    Update main.c

--- main.c
+++ main.c
@@ -1,42 +1,23 @@
 #include <stdio.h>
-#include <stdlib.h>
-#include <time.h>
-
-#define ROWS 10
-#define COLS 15
-
-int maze[ROWS][COLS];
-
-void generateMaze() {
-    srand(time(NULL));
-    for (int i = 0; i < ROWS; i++) {
-        for (int j = 0; j < COLS; j++) {
-            maze[i][j] = (rand() % 3 == 0) ? 1 : 0; // 1=牆, 0=路
-        }
-    }
-    maze[0][0] = 0;          // 起點
-    maze[ROWS-1][COLS-1] = 0; // 終點
-}
-
-void printMaze() {
-    for (int i = 0; i < ROWS; i++) {
-        for (int j = 0; j < COLS; j++) {
-            if (i == 0 && j == 0)
-                printf("S"); // Start
-            else if (i == ROWS-1 && j == COLS-1)
-                printf("E"); // End
-            else if (maze[i][j] == 1)
-                printf("#"); // Wall
-            else
-                printf("."); // Path
-        }
-        printf("\n");
-    }
-}
 
 int main() {
-    generateMaze();
-    printMaze();
-    printf("\nThis is your random maze!\n");
+    char op;
+    double num1, num2;
+
+    printf("Enter operation (+,-,*,/): ");
+    scanf(" %c", &op);
+    printf("Enter two numbers: ");
+    scanf("%lf %lf", &num1, &num2);
+
+    switch(op) {
+        case '+': printf("Result: %.2lf\n", num1+num2); break;
+        case '-': printf("Result: %.2lf\n", num1-num2); break;
+        case '*': printf("Result: %.2lf\n", num1*num2); break;
+        case '/': 
+            if(num2!=0) printf("Result: %.2lf\n", num1/num2);
+            else printf("Cannot divide by zero!\n");
+            break;
+        default: printf("Invalid operator!\n");
+    }
     return 0;
 }
